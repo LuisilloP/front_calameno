8de2f814c0c9da9995c75a7df8e18ede526e3516
LuisilloP <luisaraya98@hotmail.com>
2025-11-06 17:42:10 -0300

    add bars

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,27 +2,13 @@
 
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useAuth } from "../../context/AuthContext";
 
 export default function DashboardPage() {
-  const { user, loading, logout } = useAuth();
   const router = useRouter();
 
-  useEffect(() => {
-    if (!loading && !user) {
-      router.replace("/login");
-    }
-  }, [loading, user, router]);
-
-  if (loading || !user) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <main style={{ padding: 24 }}>
+    <main style={{ padding: 24 }} className="bg-gray-800">
       <h1>Dashboard</h1>
-      <p>Bienvenido, {user.name}!</p>
-      <button onClick={logout}>Cerrar Sesión</button>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
-import { SidebarNavigation, SidebarNavigationProps } from "@/components/ui/SidebarNavigation";
+import {
+  SidebarNavigation,
+  SidebarNavigationProps,
+} from "@/components/ui/SidebarNavigation";
+import { BarChart } from "../components/graphics/Bar_one";
 import {
   ArrowUpDownIcon,
   CalendarIcon,
@@ -18,84 +22,105 @@ import {
   Users2,
   WarehouseIcon,
 } from "lucide-react";
+import { DoughnutChart } from "@/components/graphics/Bar_two";
 
 export default function Home() {
+  const sidebarDemoProps: SidebarNavigationProps = {
+    variant: "dark",
+    brand: {
+      name: "Sistema de Gestión Cocina",
+      tagline: "Angel Calameño",
+      badge: "0.1 Beta",
+      badgeHref: "#",
+    },
 
-    const sidebarDemoProps: SidebarNavigationProps = {
-      variant: "dark",
-      brand: {
-        name: "Sistema de Gestión Cocina",
-        tagline: "Angel Calameño",
-        badge: "0.1 Beta",
-        badgeHref: "#",
+    sections: [
+      {
+        title: "Funciones Principales",
+        id: "main",
+        items: [
+          {
+            id: "dashboard",
+            label: "Panel",
+            icon: LayoutDashboardIcon,
+            href: "#",
+          },
+          {
+            id: "inventory",
+            label: "Inventario",
+            icon: WarehouseIcon,
+            href: "#",
+          },
+          {
+            id: "locations",
+            label: "Ubicaciones",
+            icon: LandPlotIcon,
+            href: "#",
+          },
+          { id: "person", label: "Personas", icon: Users2, href: "#" },
+          {
+            id: "help",
+            label: "Ayuda",
+            icon: CircleQuestionMarkIcon,
+            href: "#",
+          },
+        ],
       },
-
-      sections: [
-        {
-          title: "Funciones Principales",
-          id: "main",
-          items: [
-            {
-              id: "dashboard",
-              label: "Panel",
-              icon: LayoutDashboardIcon,
-              href: "#",
-            },
-            {
-              id: "inventory",
-              label: "Inventario",
-              icon: WarehouseIcon,
-              href: "#",
-            },
-            {
-              id: "locations",
-              label: "Ubicaciones",
-              icon: LandPlotIcon,
-              href: "#",
-            },
-            { id: "person", label: "Personas", icon: Users2, href: "#" },
-            {
-              id: "help",
-              label: "Ayuda",
-              icon: CircleQuestionMarkIcon,
-              href: "#",
-            },
-          ],
-        },
-        {
-          title: "Acciones",
-          id: "acions",
-          items: [
-            {
-              id: "ingresarProductos",
-              label: "Ingresar Productos",
-              icon: PackagePlusIcon,
-              href: "#",
-            },
-            {
-              id: "egresarProductos",
-              label: "Egresar Productos",
-              icon: PackageMinusIcon,
-              href: "#",
-            },
-            {
-              id: "moverProductos",
-              label: "Mover Productos",
-              icon: ArrowUpDownIcon,
-              href: "#",
-            },
-          ],
-        },
-      ],
-      user: {
-        name: "Tom Cook",
-        avatarSrc: "https://i.pravatar.cc/160?img=5",
-        caption: "View profile",
+      {
+        title: "Acciones",
+        id: "acions",
+        items: [
+          {
+            id: "ingresarProductos",
+            label: "Ingresar Productos",
+            icon: PackagePlusIcon,
+            href: "#",
+          },
+          {
+            id: "egresarProductos",
+            label: "Egresar Productos",
+            icon: PackageMinusIcon,
+            href: "#",
+          },
+          {
+            id: "moverProductos",
+            label: "Mover Productos",
+            icon: ArrowUpDownIcon,
+            href: "#",
+          },
+        ],
       },
-    };
+    ],
+    user: {
+      name: "Tom Cook",
+      avatarSrc: "https://i.pravatar.cc/160?img=5",
+      caption: "View profile",
+    },
+  };
   return (
-    <main className="">
-      <SidebarNavigation {...sidebarDemoProps}/>
+    <main className="bg-gray-900">
+      <div className=" flex gap-2">
+        <SidebarNavigation {...sidebarDemoProps} />
+        <div>
+          <p></p>
+        </div>
+        <div className="grid grid-cols-2">
+          <BarChart
+            title="Top 10 productos"
+            sortDirection="desc"
+            limit={10}
+            height={500}
+            dataField={"id"}
+          ></BarChart>
+          <DoughnutChart
+            title="Distribución por Stock Mínimo"
+            dataField="minStock"
+            sortDirection="desc"
+            limit={10}
+            height={400}
+          />
+        </div>
+      </div>
     </main>
   );
 }
